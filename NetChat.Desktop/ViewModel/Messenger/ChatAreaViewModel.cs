8afcf2763a559fbe89316393b7b80f4cdd5af18b
RinKunn <girfanov93@gmail.com>
2021-01-21 19:05:45 +0300

    Added NewMessages count

--- NetChat.Desktop/ViewModel/Messenger/ChatAreaViewModel.cs
+++ NetChat.Desktop/ViewModel/Messenger/ChatAreaViewModel.cs
@@ -2,7 +2,9 @@
 using System.Collections.ObjectModel;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Threading;
 using NetChat.Desktop.Services.Messaging;
 using NetChat.Desktop.Services.Messaging.Messages;
@@ -17,11 +19,49 @@ namespace NetChat.Desktop.ViewModel.Messenger
         private IReceiverHub _receiverHub;
         private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
 
+        private int _lastVisibleMessageIndex;
+        public int LastVisibleMessageIndex
+        {
+            get => _lastVisibleMessageIndex;
+            set
+            {
+                Set(ref _lastVisibleMessageIndex, value);
+                NewMessagesCount = Math.Min(NewMessagesCount, _messages.Count - 1 - _lastVisibleMessageIndex);
+            }
+        }
+
+        private int _newMessagesCount;
+        public int NewMessagesCount
+        {
+            get => _newMessagesCount;
+            set => Set(ref _newMessagesCount, value);
+        }
+
         private ObservableCollection<MessageObservable> _messages;
         public ObservableCollection<MessageObservable> Messages
         {
             get => _messages;
-            private set => Set(ref _messages, value);
+            private set
+            {
+                if(_messages != null) _messages.CollectionChanged -= Messages_CollectionChanged;
+                Set(ref _messages, value);
+                if (_messages != null) _messages.CollectionChanged += Messages_CollectionChanged;
+            }
+        }
+
+        private void Messages_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if(e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+            {
+                if(((MessageObservable)e.NewItems[e.NewItems.Count - 1]).IsOriginNative)
+                {
+                    ReadAllMessages();
+                }
+                else if (LastVisibleMessageIndex < Messages.Count - 1)
+                    NewMessagesCount += e.NewItems.Count;
+            }
+            
+            
         }
 
         public ChatAreaViewModel()
@@ -41,7 +81,6 @@ namespace NetChat.Desktop.ViewModel.Messenger
             _messageLoader = messageLoader ?? throw new ArgumentNullException(nameof(messageLoader));
             _receiverHub = receiverHub ?? throw new ArgumentNullException(nameof(receiverHub));
             _receiverHub.SubscribeMessageReceived(this, HandleMessage);
-            Messages = new ObservableCollection<MessageObservable>();
         }
 
         public override void Cleanup()
@@ -65,5 +104,16 @@ namespace NetChat.Desktop.ViewModel.Messenger
             DispatcherHelper.CheckBeginInvokeOnUI(() =>
                 Messages = new ObservableCollection<MessageObservable>(loadedMessages));
         }
+
+        private ICommand _readAllMessagesCommand;
+        public ICommand ReadAllMessagesCommand => _readAllMessagesCommand ??
+            (_readAllMessagesCommand = new RelayCommand(ReadAllMessages));
+
+        private void ReadAllMessages()
+        {
+            LastVisibleMessageIndex = _messages.Count - 1;
+        }
+
+            
     }
 }
