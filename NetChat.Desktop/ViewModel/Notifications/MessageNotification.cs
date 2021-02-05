348495227eaa0e0ae045d685c696c18ca62b6cc6
RinKunn <girfanov93@gmail.com>
2021-02-05 18:18:36 +0300

    Adding notifications..

---
+++ NetChat.Desktop/ViewModel/Notifications/MessageNotification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetChat.Desktop.ViewModel.Notifications
+{
+    public class MessageNotification : NotificationBase
+    {
+        public MessageNotification(string sender, string message) : base("Новое сообщение", sender, message)
+        {
+
+        }
+    }
+}
---
+++ NetChat.Desktop/ViewModel/Notifications/ParticipantNotification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetChat.Desktop.ViewModel.Notifications
+{
+    public class ParticipantNotification : NotificationBase
+    {
+        public ParticipantNotification(string participantName, bool isOnline)
+            : base(null, participantName, isOnline ? "В сети" : "Ушел из сети")
+        {
+
+        }
+    }
+}
